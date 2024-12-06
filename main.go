4e0996589547297822dbf3d1439f9e06ad281bb0
IVO <ivo_vilar@hotmail.com>
2024-12-05 22:00:36 -0300

    add logger

--- main.go
+++ main.go
@@ -1,10 +1,21 @@
 package main
 
 import (
+	"github.com/ivovilar1/goledger-challenge-besu/config"
 	"github.com/ivovilar1/goledger-challenge-besu/router"
 )
 
+var (
+	logger *config.Logger
+)
+
 func main() {
+	logger = config.GetLogger("main")
+	err := config.InitDatabase()
+	if err != nil {
+		logger.Err(err)
+		return
+	}
 
 	router.RunServerAndRoutes()
 
