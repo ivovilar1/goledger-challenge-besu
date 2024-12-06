f6191abfefda395534f433b169c44dc4db751003
IVO <ivo_vilar@hotmail.com>
2024-12-06 19:07:37 -0300

    add contractresponse

--- schemas/contract.go
+++ schemas/contract.go
@@ -1,10 +1,19 @@
 package schemas
 
 import (
+	"time"
+
 	"gorm.io/gorm"
 )
 
 type Contract struct {
 	gorm.Model
 	Value string
+}
+
+type ContractResponse struct {
+	ID        uint      `json:"id"`
+	Value     string    `json:"value"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
 }
